8a04f639ee6add84ca60ba0be6da9583f0ec0f07
孟政元 <278227739@qq.com>
2017-12-03 09:21:53 +0800

    finish v1.0.0

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const questions = require('./lib/questions')
+const config = require('./lib/config')
+
+module.exports = function(port)
+{
+    config.port = port || config.port
+    return {
+        addQuestion: questions.addQuestion
+    }
+}
+
 require('./lib/entry')
---
+++ test.js
@@ -0,0 +1,11 @@
+const app = require('./')()
+
+app.addQuestion
+(
+    {
+        question: '________, 彬彬有礼',
+        tip: '南湖公园长椅',
+        answer: '婉婉有仪'
+    },
+)
+
